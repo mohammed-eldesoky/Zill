71a40926c7e8907ff0ab46f78218dfd13f0e69b0
mohamed <mohammedeldesoky77@gmail.com>
2025-12-15 01:20:15 +0200

    feat (routing ) implement routing [AUTH LAYOUT , MAIN LAYOUT]

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,72 @@
 import { Routes } from '@angular/router';
 
 
+import { AuthLayoutComponent } from './core/layouts/auth-layout/auth-layout.component';
+import { MainLayoutComponent } from './core/layouts/main-layout/main-layout.component';
 
 export const routes: Routes = [
 
+  /* ======================= AUTH LAYOUT ======================== */
+    
+   
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./core/auth/login/login.component')
+            .then(c => c.LoginComponent),
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./core/auth/signup/signup.component')
+            .then(c => c.SignupComponent),
+      },
+    ],
+  },
+
+//==============   MAIN LAYOUT================
+  
+  
+  {
+    path: 'app',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./features/home/home.component')
+            .then(c => c.HomeComponent),
+      },
+      {
+        path: 'messages',
+        loadComponent: () =>
+          import('./features/messages/messages.component')
+            .then(c => c.MessagesComponent),
+      },
+      {
+        path: 'messages/:id',
+        loadComponent: () =>
+          import('./features/specific/specific.component')
+            .then(c => c.SpecificComponent),
+      },
+      {
+        path: 'user',
+        loadComponent: () =>
+          import('./features/user/user.component')
+            .then(c => c.UserComponent),
+      },
+    ],
+  },
+
+  /* ======================= FALLBACK ======================== */
+     
+
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
--- src/app/core/layouts/main-layout/main-layout.component.ts
+++ src/app/core/layouts/main-layout/main-layout.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../../features/navbar/navbar.component";
+import { FooterComponent } from "../../../features/footer/footer.component";
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  imports: [NavbarComponent, FooterComponent, RouterOutlet],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
