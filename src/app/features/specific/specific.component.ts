bb654c9ed0a054dd8df895e51713dbe0ecb20bdc
mohamed <mohammedeldesoky77@gmail.com>
2025-12-14 21:09:06 +0200

    first commit

---
+++ src/app/features/specific/specific.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-specific',
+  imports: [],
+  templateUrl: './specific.component.html',
+  styleUrl: './specific.component.scss'
+})
+export class SpecificComponent {
+
+}
