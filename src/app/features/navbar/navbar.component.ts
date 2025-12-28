e15617f5ae24c843b697bfa093415a2a91aa8b4b
mohamed <mohammedeldesoky77@gmail.com>
2025-12-28 17:27:32 +0200

    `Updated navbar component HTML and TS files with minor changes and added conditional logic for user authentication`

--- src/app/features/navbar/navbar.component.ts
+++ src/app/features/navbar/navbar.component.ts
@@ -16,7 +16,7 @@ export class NavbarComponent implements OnInit {
   dir: any;
   profileUrl: string | null = null;
 
-  constructor(private auth: AuthService, private userService: UserService) {}
+  constructor(public auth: AuthService, private userService: UserService) {}
 
   ngOnInit(): void {
     console.log(this.auth.getUserData());
